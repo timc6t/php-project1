6d242dd412ad7e03c119b71d3e5465be4519b860
Tim <tim.casiano7@gmail.com>
2024-12-09 19:15:23 +0100

    Adding the names of the employees to the expenses

--- main.php
+++ main.php
@@ -2,6 +2,11 @@
     require 'sessions.php';
     require_once 'db_config.php';
     check_session();
+
+    $res = load_config(dirname(__FILE__) . "/configuration.xml", dirname(__FILE__) . "/configuration.xsd"); 
+    $cadena_conexion = 'mysql:dbname=employee_expenses;host=127.0.0.1'; 
+    $usuario = 'root'; 
+    $clave = ''; 
 ?>
 
 <!DOCTYPE html>
@@ -10,37 +15,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main page</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <?php require 'header.php'; ?>
     <h1>Your expenses</h1>
 
-    <!-- List here all of the expenses that the employee has -->
+    <!-- TODO: List here all of the expenses that the employee has. -->
+    <!-- For now it only shows all the expenses from all the employees. -->
 
-    <table>
-		<tr><th>Category</th><th>Description</th><th>Report date</th><th>Amount</th><th>Status</th><th>Created at</th></tr>		
+    <table class="table-style">
+		<tr>
+            <th>Employee</th>
+            <th>Category</th>
+            <th>Description</th>
+            <th>Report date</th>
+            <th>Amount</th>
+            <th>Status</th>
+            <th>Created at</th>
+        </tr>		
 		<?php
-            $res = load_config(dirname(__FILE__) . "/configuration.xml", dirname(__FILE__) . "/configuration.xsd"); 
-            $cadena_conexion = 'mysql:dbname=employee_expenses;host=127.0.0.1'; 
-            $usuario = 'root'; 
-            $clave = ''; 
             try { 
                 $bd = new PDO($cadena_conexion, $usuario, $clave); 				
-                $sql = 'SELECT * FROM expenses'; 
+                $sql = 'SELECT * FROM expenses JOIN users ON expenses.user_id = users.user_id'; 
                 $users = $bd->query($sql); 				
                 foreach ($users as $usu) {
                     //$rep_id = $usu['report_id'];
                     $user_id = $usu['user_id'];
+                    $name = $usu['name'];
                     $cat = $usu['category'];
                     $desc = $usu['description'];
                     $repDate = $usu['report_date'];
                     $amount = $usu['amount'];
                     $status = $usu['status'];
                     $created = $usu['created_at'];
-                    echo "<tr><td>$cat</td><td>$desc</td><td>$repDate</td><td>$amount</td><td>$status</td><td>$created</td></tr>";
+                    echo "<tr><td>$name</td><td>$cat</td><td>$desc</td><td>$repDate</td><td>$amount</td><td>$status</td><td>$created</td></tr>";
                 }
             } catch (PDOException $e) { 
-                echo 'Databse error: ' . $e->getMessage(); 
+                echo 'Database error: ' . $e->getMessage(); 
             }
 		?>
 	</table>
